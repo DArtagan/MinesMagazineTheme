9a7f7879f66e638f697ada7141ba191062caa861
Nick Sutcliffe <magazine@mines.edu>
2012-11-19 17:39:53 -0600

    MinesMagazine.com server based commits

--- deploy.php
+++ deploy.php
@@ -11,8 +11,11 @@
 $commands = array(
 'echo $PWD',
 'whoami',
-'git pull',
 'git status',
+'git add -A 2>&1',
+'git commit -m "MinesMagazine.com server based commits" 2>&1',
+'git push origin master',
+'git pull',
 'git submodule sync',
 'git submodule update',
 'git submodule status',
