6b7bbf471aefcea066db02eacc152c4ab505f0a8
William Weiskopf <william@weiskopf.me>
2013-06-08 23:37:39 -0600

    Functional child theme

--- sidebar.php
+++ sidebar.php
@@ -1,16 +1,35 @@
 <!-- SIDEBAR -->
-<?php include (TEMPLATEPATH.'/tools/get-theme-options.php'); ?>
 <div id="sidebar">
   <div id="sidelist">
-  
+  	 <?php if (!prinz_get_option('prinz_newsblock')) : ?>
       <?php if ( is_home() ) { ?>
-    <div>
-      <h1 style="text-align: center;"><?php CurrentIssue() ?></h1>
+    <div class="newsblock">
+      <h3>
+        <?php 
+	// this is where the name of the News (or whatever) category gets printed	  
+	wp_list_categories('include='.get_cat_id(prinz_get_option('prinz_lead')).'&title_li=&style=none'); ?>
+      </h3>
+      <?php 
+// this is where the last five headlines are pulled from the News (or whatever) category but not the first one (offset=1) since this is already displayed as leadarticle. 
+		query_posts('cat='.get_cat_id(prinz_get_option('prinz_lead')).'&offset=1&showposts='.prinz_get_option('prinz_sidenewsnumber'));
+		?>
+      <ul class="bullets">
+        <?php while (have_posts()) : the_post(); ?>
+        <li><a href="<?php the_permalink() ?>" rel="bookmark">
+          <?php the_title(); ?>
+          </a></li>
+        <?php endwhile; ?>
+      </ul>
     </div>
     <?php } ?>
+  <?php endif; ?>
+
   
-    <?php 	/* Widgetized sidebar, if you have the plugin installed. */
-					if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Regular Sidebar') ) : ?>
+   <?php if ( is_active_sidebar( 'regular' ) ) : ?>
+
+		<?php dynamic_sidebar( 'regular' ); ?>
+        
+        <?php else : ?>
       
     <?php
 // this is where 10 headlines from the current category get printed	  
@@ -20,7 +39,7 @@ $categories = get_the_category();
 foreach ($categories as $category) :
 ?>
     <div>
-      <h3><?php _e('More from this category','branfordmagazine');?></h3>
+      <h3><?php _e('More from this category', PRiNZ_DOMAIN);?></h3>
       <ul class="bullets">
         <?php
 $posts = get_posts('numberposts=10&category='. $category->term_id);
@@ -34,40 +53,18 @@ foreach($posts as $post) :
     </div>
     <?php endforeach; endif ; ?>
 
-    <div>
-      <h3><?php _e('Browse Categories','branfordmagazine');?></h3>
-      <ul class="subnav">
-       <?php wp_list_categories("exclude=$prinz_sidecats&orderby=name&title_li=&hide_empty=1"); ?>
-       	    
+  <?php endif; ?>
+  
+  <?php if (!prinz_get_option('prinz_stayinformed')) : ?>
+      <div>
+      <h3><?php _e('Stay informed', PRiNZ_DOMAIN);?></h3>
+      <ul class="feed">
+        <li><a href="<?php bloginfo('rss2_url'); ?>"><?php _e('Entries RSS', PRiNZ_DOMAIN);?></a></li>
+        <li><a href="<?php bloginfo('comments_rss2_url'); ?>"><?php _e('Comments RSS', PRiNZ_DOMAIN);?></a></li>
+        <li class='podcast'><a href='http://itunes.apple.com/us/podcast/mines-magazine/id303361339'>Subscribe to our Podcast (iTunes)</a></li>
       </ul>
-    </div>
-    <div>
-      <h3><?php _e('Ads &amp; Sponsors','branfordmagazine');?></h3>
-<!-- GOOGLE 250x250px ADSENSE GOES HERE -->
-<?php if ('wp_flash_img_show()') {wp_flash_img_show();} ?>
-<?php echo stripslashes($prinz_250x250_ads); ?>
-<!-- END GOOGLE ADSENSE -->
-     </div>
-    <div>
-      <h3><?php _e('Browse Archives','branfordmagazine');?></h3>
-     <select name="archive-dropdown" onchange='document.location.href=this.options[this.selectedIndex].value;'> 
-  <option value=""><?php echo attribute_escape(__('Select Month')); ?></option> 
-  <?php wp_get_archives('type=monthly&format=option&show_post_count=0'); ?> </select>
-
     </div>
   <?php endif; ?>
-        <div>
-      <h3><?php _e('Stay informed','branfordmagazine');?></h3>
-      <ul>
-        <li class="feed"><a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a></li>
-        <li class="feed"><a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a></li>
-		<li class="podcast"><a href="http://itunes.apple.com/us/podcast/mines-magazine/id303361339">Subscribe to our Podcast (iTunes)</a></li>
-      </ul>
-    </div>
-
-	
-	<!--<a href="http://itunes.apple.com/us/podcast/mines-magazine/id303361339"><img src="http://magazine.mines.edu/NewSite/Pictures/podcast.jpg" />&nbsp;Check out our iTunes podcast</a> | <a href="http://minesmagazine.com/?page_id=2490">Access audio files</a>-->
-<!--<a href=http://itunes.apple.com/us/podcast/mines-magazine/id303361339 rel=rokbox[1024][768]><img src= http://magazine.mines.edu/NewSite/Pictures/podcast.jpg>Subscribe to our Podcast (iTunes)</a>-->
 	
   </div>
   <!--END SIDELIST-->
