6ae743ab9d8d6dda02f39ebe2b78a4a7921b232d
Mines Magazine <magazine@mines.edu>
2013-06-09 00:37:51 -0500

    MinesMagazine.com server based commits

--- functions.php
+++ functions.php
@@ -89,8 +89,8 @@ class widget_wp_flash_img_show extends WP_Widget {
 		}
 		?>
 		<p>
-		<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label> 
-		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
 		</p>
 		<?php 
 	}
