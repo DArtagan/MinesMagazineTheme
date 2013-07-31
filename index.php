0e9fccb6a98d4408caf79be2561025640513d97e
William Weiskopf <william@weiskopf.me>
2013-07-31 02:30:57 -0600

    Content with the responsive theme for the night

--- index.php
+++ index.php
@@ -42,11 +42,6 @@
     )
   );
   $featured_query = new WP_Query( $args ); ?>
-    <h4>
-      <?php 
-	// name of the "featured articles" category gets printed	  
-	wp_list_categories('include='.get_cat_id(prinz_get_option('prinz_featured')).'&title_li=&style=none'); ?>
-    </h4>
     <?php while ($featured_query->have_posts()) : $featured_query->the_post(); ?>
     <div class="feature">
       <h3><a href="<?php the_permalink() ?>" rel="bookmark" class="title">
