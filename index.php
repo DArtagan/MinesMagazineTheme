57cc73a0c86f52968a3e3a469fbd1c4489254599
William Weiskopf <william@weiskopf.me>
2013-08-19 21:48:13 -0600

    3 column layout

--- index.php
+++ index.php
@@ -3,27 +3,32 @@
 
 <div id="content" class="clearfloat">
 
-  <div id="feature" class="column">
-    <?php MM_homepageBox(MM_homepageQuery( 'feature' )); ?>
-  </div>
+  <div id="twocol">
+    <div id="feature" class="column">
+      <?php MM_homepageBox(MM_homepageQuery( 'feature' )); ?>
+    </div>
 
-  <div>
-    <?php if ( is_active_sidebar( 'belowtabbed' ) ) : // Widgetized area below the tabbed content
-      dynamic_sidebar( 'belowtabbed' );
-      else :
-      endif; 
-    ?>
-  </div>
+    <div>
+      <?php if ( is_active_sidebar( 'belowtabbed' ) ) : // Widgetized area below the tabbed content
+        dynamic_sidebar( 'belowtabbed' );
+        else :
+        endif; 
+      ?>
+    </div>
 
-  <div id="leftcolumn" class="column">
-    <?php MM_homepageBox(MM_homepageQuery( 'left' )); ?>
-  </div>
+    <div id="leftcolumn" class="column">
+      <?php MM_homepageBox(MM_homepageQuery( 'left' )); ?>
+    </div>
 
-  <div id="centercolumn" class="column">
-    <?php MM_homepageBox(MM_homepageQuery( 'center' )); ?>
+    <div id="centercolumn" class="column">
+      <?php MM_homepageBox(MM_homepageQuery( 'center' )); ?>
+    </div>
   </div>
 
   <div id="rightcolumn" class="column">
+    <div>
+      <h1 style="text-align: center;"><?php echo MM_currentIssue() ?></h1>
+    </div>
     <?php get_sidebar(); ?>
     <?php MM_homepageBox(MM_homepageQuery( 'right' )); ?>
   </div>
