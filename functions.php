d45b0f2459c2ac3bf29a87d049c06fdd65858188
William Weiskopf <william@weiskopf.me>
2013-10-21 01:15:08 -0600

    Child categories now will pull from parent template.

--- functions.php
+++ functions.php
@@ -486,4 +486,38 @@ include('includes/homepageSetup.php');
     	wp_reset_query();
 	}
 
+/**
+ * Force sub-categories to use the parent category template
+ * By: Drew Jaynes
+ * http://werdswords.com/force-sub-categories-use-the-parent-category-template/
+ */
+	function new_subcategory_hierarchy() { 
+	    $category = get_queried_object();
+	 
+	    $parent_id = $category->category_parent;
+	 
+	    $templates = array();
+	     
+	    if ( $parent_id == 0 ) {
+	        // Use default values from get_category_template()
+	        $templates[] = "category-{$category->slug}.php";
+	        $templates[] = "category-{$category->term_id}.php";
+	        $templates[] = 'category.php';     
+	    } else {
+	        // Create replacement $templates array
+	        $parent = get_category( $parent_id );
+	 
+	        // Current first
+	        $templates[] = "category-{$category->slug}.php";
+	        $templates[] = "category-{$category->term_id}.php";
+	 
+	        // Parent second
+	        $templates[] = "category-{$parent->slug}.php";
+	        $templates[] = "category-{$parent->term_id}.php";
+	        $templates[] = 'category.php'; 
+	    }
+	    return locate_template( $templates );
+	}
+	 
+	add_filter( 'category_template', 'new_subcategory_hierarchy' );
 ?>
